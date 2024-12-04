8a79095c90254f56ff6b8700f2bafb79acd72454
Tor Anders Gustavsen <tor.anders@arkitektum.no>
2024-12-04 08:49:08 +0100

    Fixed the Socket IO server  environment variable

--- src/dokanalyse/utils/socket_io.py
+++ src/dokanalyse/utils/socket_io.py
@@ -1,9 +1,9 @@
-import os
 import socketio
+from .helpers.common import get_env_var
    
 def get_client():
     try:
-        url = os.environ.get('SOCKET_IO_URL', 'http://0.0.0.0:5002')
+        url = get_env_var('SOCKET_IO_SRV_URL')
         sio = socketio.SimpleClient()
         sio.connect(url, socketio_path='/ws/socket.io')
         
